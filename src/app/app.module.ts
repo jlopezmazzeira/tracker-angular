2b094dd3d6f132b01eb448407e002775fc2d23ba
Jesus Lopez <jlopezmazzeira@gmail.com>
2018-08-02 16:00:06 -0400

    fuentes

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { AgmCoreModule } from '@agm/core';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { environment } from '../environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyAk-rD3_J8AaYd__2FcTLi_bf5CBhxxPTQ'
+    }),
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
